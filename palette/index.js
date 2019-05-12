ba6297a93fcd96030a4a3da9b38852d455a742c3
GeorgeTuz <mih_petro@mail.ru>
2019-05-12 14:02:07 +0300

    faet: add localStorage functionality

--- palette/index.js
+++ palette/index.js
@@ -1,40 +1,49 @@
 
-var curent_color=window.getComputedStyle(document.getElementById('current')).getPropertyValue('background-color');
+var curentColor=window.getComputedStyle(document.getElementById('current')).getPropertyValue('background-color');
 var cursor;
 
-function zalivka_cursor() {
-    document.body.style.cssText='cursor: url(palette/assets/img/i1.png), pointer;';
-    cursor="zalivka";
+if (localStorage.getItem("curColor") != null) {
+    curentColor=localStorage.getItem("curColor");
+    document.getElementById('current').style.backgroundColor = curentColor;
 }
 
-function pipetka_cursor() {
-    document.body.style.cssText='cursor: url(palette/assets/img/i2.png), pointer;';
-    cursor="pipetka";
+if (localStorage.getItem("prevColor") != null) {
+    document.getElementById('prev').style.backgroundColor = localStorage.getItem("prevColor");
 }
 
-function move_cursor() {
-    document.body.style.cssText='cursor: url(palette/assets/img/i3.png), pointer;';
+function bucketCursor() {
+    document.body.style.cssText='cursor: url(palette/assets/img/Bucket.png), pointer;';
+    cursor="bucket";
+}
+
+function chooseColorCursor() {
+    document.body.style.cssText='cursor: url(palette/assets/img/ChooseColor.png), pointer;';
+    cursor="chooseColor";
+}
+
+function moveCursor() {
+    document.body.style.cssText='cursor: url(palette/assets/img/Move.png), pointer;';
     cursor="move";
 }
 
-function transform_cursor() {
-    document.body.style.cssText='cursor: url(palette/assets/img/i4.png), pointer;';
+function transformCursor() {
+    document.body.style.cssText='cursor: url(palette/assets/img/Transform.png), pointer;';
     cursor="transform";
 }
 
 function moveRect(e){        
     switch(e.keyCode){     
         case 49: 
-            zalivka_cursor();
+            bucketCursor();
             break;
         case 50: 
-            pipetka_cursor();
+            chooseColorCursor();
             break;
         case 51:  
-            move_cursor();
+            moveCursor();
             break;
         case 52: 
-            transform_cursor();
+            transformCursor();
             break;
     }
 }
@@ -44,26 +53,78 @@ var colors = document.getElementsByClassName("color");
 for (var i = 0; i < colors.length; i++) {
     colors[i].onclick = function(e) {
         color = document.getElementById(this.id);
-        if (cursor=="pipetka") {
-            document.getElementById('prev').style.backgroundColor = curent_color;
+        if (cursor=="chooseColor") {
+            document.getElementById('prev').style.backgroundColor = curentColor;
+            localStorage.setItem("prevColor", curentColor);
             document.getElementById('current').style.backgroundColor = window.getComputedStyle(color).getPropertyValue('background-color');
-            curent_color = window.getComputedStyle(document.getElementById('current')).getPropertyValue('background-color');
+            curentColor = window.getComputedStyle(document.getElementById('current')).getPropertyValue('background-color');
+            localStorage.setItem("curColor", curentColor);
         }    
     }
 }
 
+var el = document.getElementById('canvas');
+var posCanvasLeft = findPosX(el);
+var posCanvasTop = findPosY(el);
+ 
+function findPosX(obj) {
+    var curleft = 0;
+    if (obj.offsetParent) {
+        while (1) {
+            curleft+=obj.offsetLeft;
+            if (!obj.offsetParent) {
+                break;
+            }
+            obj=obj.offsetParent;
+        }
+    } else if (obj.x) {
+        curleft+=obj.x;
+    }
+    return curleft;
+}
+ 
+function findPosY(obj) {
+    var curtop = 0;
+    if (obj.offsetParent) {
+        while (1) {
+            curtop+=obj.offsetTop;
+            if (!obj.offsetParent) {
+                break;
+            }
+            obj=obj.offsetParent;
+        }
+    } else if (obj.y) {
+        curtop+=obj.y;
+    }
+    return curtop;
+}
+
 var elements = document.getElementsByClassName("block");
 for (var i = 0; i < elements.length; i++) {
+    if (localStorage.getItem(elements[i].id+'Color') != null) {
+        document.getElementById(elements[i].id).style.backgroundColor = localStorage.getItem(elements[i].id+'Color');
+    }
+    if (localStorage.getItem(elements[i].id+'Radius') != null) {
+        document.getElementById(elements[i].id).style.borderRadius = localStorage.getItem(elements[i].id+'Radius');
+    }
+    if (localStorage.getItem(elements[i].id+'Top') != null) {
+        document.getElementById(elements[i].id).style.top = localStorage.getItem(elements[i].id+'Top');
+        document.getElementById(elements[i].id).style.left = localStorage.getItem(elements[i].id+'Left');
+    }
     elements[i].onmousedown = function(e) {
         block = document.getElementById(this.id);
-        if (cursor=="zalivka") {
-            block.style.backgroundColor = curent_color;
+        if (cursor=="bucket") {
+            block.style.backgroundColor = curentColor;
+            localStorage.setItem(this.id+'Color', curentColor);
         }
         if (cursor=="transform") {
             if (block.style.borderRadius == '85px'){
                 block.style.borderRadius = '0px';
+                localStorage.setItem(this.id+'Radius', '0px');
+                
             } else {
                 block.style.borderRadius = '85px';
+                localStorage.setItem(this.id+'Radius', '85px');
             }
         }
         if (cursor=="move") {
@@ -87,6 +148,15 @@ for (var i = 0; i < elements.length; i++) {
             };
         
             block.onmouseup = function() {
+                var leftKey = window.getComputedStyle(block).getPropertyValue('left');
+                leftKey = leftKey.slice(0, -2);
+                leftKey = Math.round((+leftKey - posCanvasLeft + 1) * 10) / 10 + 'px';
+                localStorage.setItem(this.id+'Left', leftKey);
+                var topKey = window.getComputedStyle(block).getPropertyValue('top');
+                topKey = topKey.slice(0, -2);
+                topKey = Math.round((+topKey - posCanvasTop) * 10) / 10 + 'px';
+                localStorage.setItem(this.id+'Top', topKey);
+
                 document.onmousemove = null;
                 block.onmouseup = null;
             };
